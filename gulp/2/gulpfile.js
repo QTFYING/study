2e3c357c1577760a0e63dc7b7c940e0035b207c6
Virgo <2811132560@qq.com>
2024-12-03 18:07:16 +0800

    gulp/2 调整gulpfile配置，使其支持自动刷新

--- gulp/2/gulpfile.js
+++ gulp/2/gulpfile.js
@@ -1,18 +1,22 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const path = require('path');
+const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify');
 
 // 定义编译 JavaScript 文件的任务
-gulp.task('compile-js', function () {
-  return gulp.src('src/index.js')
+gulp.task('scripts', function () {
+  return gulp.src('src/*.js')
+    .pipe(babel()) // 使用 .babelrc 中的配置
     .pipe(uglify()) // 压缩文件
-    .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
 
-// 定义一个默认任务，它会在你运行 `gulp` 命令时执行
-gulp.task('default', gulp.series('compile-js', function(done) {
-  console.log('Compilation tasks completed!');
-  done();
+gulp.task('serve', gulp.series('scripts', function () {
+    browserSync.init({
+      server: { baseDir: './'}
+  });
+  gulp.watch('src/*.js', gulp.series('scripts')).on('change', browserSync.reload);
+  gulp.watch('*.html').on('change', browserSync.reload);
 }));
+
+gulp.task('default', gulp.series('serve'));
