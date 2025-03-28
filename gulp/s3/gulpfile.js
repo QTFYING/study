61a51fb69ef5b12f8c07183101e32b319f0120b9
yapeng.ma <yapeng.ma@ly.com>
2025-03-28 14:54:25 +0800

    aws-s3增加空包请求功能

--- gulp/s3/gulpfile.js
+++ gulp/s3/gulpfile.js
@@ -2,6 +2,9 @@ const { series, watch } = require('gulp');
 const gulp = require('gulp');
 const path = require('path');
 const plumber = require('gulp-plumber');
+const fs = require('fs');
+const https = require('https');
+const unzipper = require('unzipper');
 
 // 配置对象
 const config = {
@@ -9,7 +12,58 @@ const config = {
   destDir: 'static',
   htmlDestDir: path.join('static', 'html'),
   jsDestDir: path.join('static', 'js'),
+  zipUrl: 'https://package.itcjf.com/packages/tibet-airline-oss/1686808649248/tibet-airline-oss-1686808649248.zip', // 替换为实际的 ZIP 包 URL
+  zipDest: 'temp.zip', // 临时存储 ZIP 包的路径
 };
+
+// 检查 dist 目录是否存在
+async function ensureDistExists(cb) {
+  if (!fs.existsSync(config.srcDir)) {
+    console.log('dist 目录不存在，开始下载 ZIP 包...');
+    await downloadZip(config.zipUrl, config.zipDest);
+    console.log('ZIP 包下载完成，开始解压...');
+    await extractZip(config.zipDest, config.srcDir);
+    console.log('解压完成');
+    fs.unlinkSync(config.zipDest); // 删除临时 ZIP 文件
+  } else {
+    console.log('dist 目录已存在，跳过下载');
+  }
+  cb();
+}
+
+// 下载 ZIP 包
+function downloadZip(url, dest) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(dest);
+    https.get(url, (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close(() => {
+          // 检查文件是否是有效的ZIP文件
+          if (fs.existsSync(dest) && fs.statSync(dest).size > 0) {
+            console.log('ZIP 文件下载成功：', dest)
+            resolve();
+          } else {
+            reject(new Error('下载的 ZIP 文件为空或损坏'))
+          }
+        });
+      });
+    }).on('error', (err) => {
+      fs.unlinkSync(dest); // 删除部分下载的文件
+      reject(err);
+    });
+  });
+}
+
+// 解压 ZIP 包
+function extractZip(zipPath, destDir) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(zipPath)
+      .pipe(unzipper.Extract({ path: destDir }))
+      .on('close', resolve)
+      .on('error', reject);
+  });
+}
 
 // 清理目标目录
 async function clean(cb) {
@@ -41,4 +95,4 @@ function watchFiles() {
 }
 
 // 默认任务
-exports.default = series(clean, htmlPack, staticPack, watchFiles);
+exports.default = series(ensureDistExists, clean, htmlPack, staticPack);
