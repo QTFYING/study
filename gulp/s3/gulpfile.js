18091143af92db45cc5b1d8d56e54c3585bad573
yapeng.ma <yapeng.ma@ly.com>
2025-03-28 14:30:13 +0800

    优化aws-s3的gulp代码

--- gulp/s3/gulpfile.js
+++ gulp/s3/gulpfile.js
@@ -1,39 +1,44 @@
-const { series } = require('gulp');
+const { series, watch } = require('gulp');
 const gulp = require('gulp');
-const fs = require('fs');
 const path = require('path');
+const plumber = require('gulp-plumber');
 
-// 定义源目录和目标目录
-const srcDir = 'dist';
-const destDir = 'static';
-const htmlDestDir = path.join(destDir, 'html');
-const jsDestDir = path.join(destDir, 'js');
+// 配置对象
+const config = {
+  srcDir: 'dist',
+  destDir: 'static',
+  htmlDestDir: path.join('static', 'html'),
+  jsDestDir: path.join('static', 'js'),
+};
 
-// 确保目标目录存在
-function ensureDirectoryExists(dirPath) {
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true });
-  }
+// 清理目标目录
+async function clean(cb) {
+  const del = (await import('del')).deleteSync; // 动态导入 del 模块
+  del([config.destDir]);
+  cb();
 }
-
-ensureDirectoryExists(htmlDestDir);
-ensureDirectoryExists(jsDestDir);
 
 // Gulp任务：打包HTML文件
 function htmlPack(cb) {
-  gulp.src(path.join(srcDir, '**/index.html'), {base: srcDir})
-    .pipe(gulp.dest(htmlDestDir))
-    .on('end', cb)
-    .on('error', cb);
+  gulp.src(path.join(config.srcDir, '**/index.html'), { base: config.srcDir })
+    .pipe(plumber())
+    .pipe(gulp.dest(config.htmlDestDir))
+    .on('end', cb);
 }
 
 // Gulp任务：打包JavaScript文件
 function staticPack(cb) {
-  gulp.src([path.join(srcDir, '**/*'), `!${path.join(srcDir, '**/*.html')}`], {base: srcDir})
-    .pipe(gulp.dest(jsDestDir))
-    .on('end', cb)
-    .on('error', cb);
+  gulp.src([path.join(config.srcDir, '**/*'), `!${path.join(config.srcDir, '**/*.html')}`], { base: config.srcDir })
+    .pipe(plumber())
+    .pipe(gulp.dest(config.jsDestDir))
+    .on('end', cb);
+}
+
+// 文件监听任务
+function watchFiles() {
+  watch(path.join(config.srcDir, '**/*.html'), htmlPack);
+  watch([path.join(config.srcDir, '**/*'), `!${path.join(config.srcDir, '**/*.html')}`], staticPack);
 }
 
 // 默认任务
-exports.default = series(htmlPack, staticPack);
+exports.default = series(clean, htmlPack, staticPack, watchFiles);
