737c0d4d339c9d98d0f2fd0c2bcb9347d152218b
Virgo <2811132560@qq.com>
2025-04-28 21:58:14 +0800

    增加angular-ui的轮播图示例

---
+++ angular/example.js
@@ -0,0 +1,36 @@
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  $scope.slides = [
+    {
+      text: 'AAA',
+      id: 0,
+      list: [{
+        title: 'A1',
+        image: 'https://unsplash.it/600/300',
+      },{
+        title: 'A2',
+        image: 'https://unsplash.it/600/300',
+      },{
+        title: 'A3',
+        image: 'https://unsplash.it/600/300',
+      }]
+    },
+    {
+      text: 'BBB',
+      id: 1,
+      list: [{
+        title: 'B1',
+        image: 'https://unsplash.it/600/300',
+      },{
+        title: 'B2',
+        image: 'https://unsplash.it/600/300',
+      },{
+        title: 'B3',
+        image: 'https://unsplash.it/600/300',
+      }]
+    }
+  ];
+});
