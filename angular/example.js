eb8e219cf50f27ac4b24c0c6448e7ba850d5ca37
Virgo <2811132560@qq.com>
2025-04-28 22:13:43 +0800

    改造成为更容易理解的二维数组

--- angular/example.js
+++ angular/example.js
@@ -4,33 +4,15 @@ angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($sc
   $scope.noWrapSlides = false;
   $scope.active = 0;
   $scope.slides = [
-    {
-      text: 'AAA',
-      id: 0,
-      list: [{
-        title: 'A1',
-        image: 'https://unsplash.it/600/300',
-      },{
-        title: 'A2',
-        image: 'https://unsplash.it/600/300',
-      },{
-        title: 'A3',
-        image: 'https://unsplash.it/600/300',
-      }]
-    },
-    {
-      text: 'BBB',
-      id: 1,
-      list: [{
-        title: 'B1',
-        image: 'https://unsplash.it/600/300',
-      },{
-        title: 'B2',
-        image: 'https://unsplash.it/600/300',
-      },{
-        title: 'B3',
-        image: 'https://unsplash.it/600/300',
-      }]
-    }
+    [
+      { title: 'A1', image: 'https://unsplash.it/600/300' },
+      { title: 'A2', image: 'https://unsplash.it/600/300' },
+      { title: 'A3', image: 'https://unsplash.it/600/300' }
+    ],
+    [
+      { title: 'B1', image: 'https://unsplash.it/600/300' },
+      { title: 'B2', image: 'https://unsplash.it/600/300' },
+      { title: 'B3', image: 'https://unsplash.it/600/300' }
+    ]
   ];
 });
