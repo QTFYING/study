d2f43f79d04052ca51ae18e3cfbced1fd4e797fc
Virgo <2811132560@qq.com>
2025-11-30 18:09:10 +0800

    feat: product产品优化

---
+++ products/电子产品成本计算器/products.js
@@ -0,0 +1,80 @@
+const product_list = [
+  {
+    "id": "1",
+    "name": "Macbook Air",
+    "purchaseDate": "2025-06-23",
+    "purchasePrice": 8299,
+    "status": "active"
+  },
+  {
+    "id": "2",
+    "name": "一加手机",
+    "purchaseDate": "2025-01-02",
+    "purchasePrice": 3199,
+    "status": "active"
+  },
+  {
+    "id": "3",
+    "name": "一加平板",
+    "purchaseDate": "2025-01-11",
+    "purchasePrice": 2230,
+    "status": "active"
+  },
+  {
+    "id": "4",
+    "name": "OPPO耳机",
+    "purchaseDate": "2025-02-19",
+    "purchasePrice": 167,
+    "status": "active"
+  },
+  {
+    "id": "5",
+    "name": "Thinkpad",
+    "purchaseDate": "2023-12-03",
+    "purchasePrice": 2988,
+    "status": "active"
+  },
+  {
+    "id": "6",
+    "name": "Macbook Pro",
+    "purchaseDate": "2023-07-22",
+    "purchasePrice": 6960,
+    "status": "active"
+  },
+  {
+    "id": "7",
+    "name": "iPhone 15",
+    "purchaseDate": "2024-05-20",
+    "purchasePrice": 5000,
+    "status": "active"
+  },
+  {
+    "id": "8",
+    "name": "小米手环",
+    "purchaseDate": "2025-06-17",
+    "purchasePrice": 309,
+    "status": "active"
+  },
+  {
+    "id": "9",
+    "name": "OPPO手环",
+    "purchaseDate": "2022-07-08",
+    "purchasePrice": 240,
+    "status": "active"
+  },
+  {
+    "id": "10",
+    "name": "OPPO电子秤",
+    "purchaseDate": "2022-11-02",
+    "purchasePrice": 110,
+    "status": "active"
+  },
+  {
+    "id": "11",
+    "name": "Macbook Pro i5",
+    "purchaseDate": "2016-10-24",
+    "purchasePrice": 8092,
+    "status": "retired",
+    "retirementDate": "2023-07-20"
+  }
+]
